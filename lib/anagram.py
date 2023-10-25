a852829ea193e0493fa3eee112fc5ee867f95538
Joseph <josephylee72@gmail.com>
2023-10-25 00:07:38 -0700

    completed anagrams

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,21 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word : str):
+        self.word = word
+
+    def match(self, list):
+        split_sorted = [*self.word]
+        split_sorted.sort()
+        print(split_sorted)
+        result = []
+        for word in list:
+            temp_split_sort = [*word]
+            temp_split_sort.sort()
+            if temp_split_sort == split_sorted:
+                result.append(word)
+        return result
+
+listen = Anagram("listen")
+
+listen.match(['enlists', 'google', 'inlets', 'banana'])
+
+# python lib/anagram.py
